f55bfafb367c3e84848610df676eb139a85739e4
Rafaela Paganotto <rafaela.paganotto@nfe.io>
2023-02-15 15:24:03 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function menuShow() {
+  let ul = document.querySelector(".navbar ul");
+
+  if (ul.classList.contains("open")) {
+    ul.classList.remove("open");
+    document.querySelector(".menu-icon img").src = "img/menu-de-hamburguer.png";
+  } else {
+    ul.classList.add("open");
+    document.querySelector(".menu-icon img").src = "img/close.png";
+  }
+}
